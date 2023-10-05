33ba7249f8da99783b7591c7bf7592411c37ad58
xhmueel <105432909+xhmueel@users.noreply.github.com>
2023-10-05 20:11:56 +0100

    exercise-2

--- engine/samples/exercises/main.cpp
+++ engine/samples/exercises/main.cpp
@@ -4,33 +4,46 @@
 #include <cubos/engine/renderer/point_light.hpp>
 #include <cubos/engine/renderer/environment.hpp>
 #include <cubos/engine/transform/plugin.hpp>
+#include <cubos/engine/settings/settings.hpp>
+#include <cubos/engine/voxels/plugin.hpp>
+#include <cubos/engine/transform/plugin.hpp>
 
 using cubos::core::ecs::Commands;
+using cubos::core::ecs::Read;
 using cubos::core::ecs::Write;
 
 
 using namespace cubos::engine;
 
+
+/// [Get handles to assets]
+static const Asset<VoxelGrid> CastleAsset = AnyAsset("cb2d193a-63b0-11ee-8c99-0242ac120002");
+static const Asset<VoxelPalette> PaletteAsset = AnyAsset("df4cdbb2-63b0-11ee-8c99-0242ac120002");
+
+// Setting the assets folder
+static void settingsSystem(Write<Settings> settings)
+{
+    settings->setString("assets.io.path", SAMPLE_ASSETS_FOLDER);
+}
+
+
 // Palette
-static void setPaletteSystem(Write<Renderer> renderer)
+static void setPaletteSystem(Read<Assets> assets, Write<Renderer> renderer)
 {
     // Create a simple palette with 3 materials (red, green and blue).
-    (*renderer)->setPalette(VoxelPalette{{
-        {{1, 0, 0, 1}},
-        {{0, 1, 0, 1}},
-        {{0, 0, 1, 1}},
-    }});
+    auto palette = assets->read(PaletteAsset);
+    (*renderer)->setPalette(*palette);
 }
 
 // Voxel Grid
-static void spawnVoxelGridSystem(Commands commands, Write<Assets> assets)
+/* static void spawnVoxelGridSystem(Commands commands, Write<Assets> assets)
 {
     // Create a 2x2x2 grid whose voxels alternate between the materials defined in the palette.
     auto gridAsset = assets->create(VoxelGrid{{2, 2, 2}, {1, 2, 3, 1, 2, 3, 1, 2}});
 
     // Spawn an entity with a renderable grid component and a identity transform.
     commands.create(RenderableGrid{gridAsset, {-1.0F, 0.0F, -1.0F}}, LocalToWorld{});
-}
+}*/
 
 // Point Light
 static void spawnLightSystem(Commands commands)
@@ -57,6 +70,15 @@ static void spawnCamerasSystem(Commands commands, Write<ActiveCameras> camera)
     camera->entities[2] = camera->entities[0];
 }
 
+static void spawnCastleSystem(Commands commands, Read<Assets> assets)
+{
+    auto castle = assets->read(CastleAsset);
+    //offset ig
+    //glm::vec3 offset = glm::vec3(castle->size().x, 0.0F, castle->size().z) / -2.0F;
+
+    commands.create().add(RenderableGrid{CastleAsset}).add(LocalToWorld{});
+}
+
 
 int main()
 {
@@ -66,10 +88,16 @@ int main()
     //Renderer plugin
     cubos.addPlugin(rendererPlugin);
 
-    cubos.startupSystem(setPaletteSystem).after("cubos.renderer.init");
-    cubos.startupSystem(spawnVoxelGridSystem);
+    cubos.addPlugin(voxelsPlugin);
+
+    cubos.startupSystem(settingsSystem).tagged("cubos.settings");
     cubos.startupSystem(spawnLightSystem);
     cubos.startupSystem(spawnCamerasSystem);
+
+    cubos.startupSystem(setPaletteSystem).after("cubos.renderer.init");
+    //cubos.startupSystem(spawnVoxelGridSystem);
+    cubos.system(spawnCastleSystem);
+    
 
     cubos.run();
 }
